52a82b36b789565d1886da0640198a1fdf63e022
Giuseppe Scalzi <giuseppe@osgate.org>
2014-10-09 16:04:50 +0200

    Working on windows decrypter

--- decrypt/decrypter.py
+++ decrypt/decrypter.py
@@ -8,7 +8,7 @@
 ###
 ####
 
-import getpass, platform, sqlite3, os, json
+import getpass, platform, sqlite3, os, json, subprocess, base64
 
 ### Constants
 
@@ -17,14 +17,17 @@ MOZ_LOGIN_FILE_DB = "signons.sqlite"
 MOZ_LOGIN_FILE_JSON = "logins.json"
 
 username = getpass.getuser()
+opsys = platform.system()
 
-GCHROME_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Local\Google\Chrome\User Data"
-FF_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Roaming\Mozilla\Firefox\Profiles"
-TH_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Roaming\Thunderbird\Profiles"
+GCHROME_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Local\Google\Chrome\User Data\Default"
+FF_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Roaming\Mozilla\Firefox"
+TH_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Roaming\Thunderbird"
 
 GCHROME_PROFILE_LINUX = "/home/" + username + "/.config/google-chrome/Default"
 FF_PROFILE_LINUX = "/home/" + username + "/.mozilla/firefox"
 TH_PROFILE_LINUX = "/home/" + username + "/.thunderbird"
+
+WIN_DECRYPTER = "C:\\Users\\John\\Documents\\Visual Studio 2012\\Projects\\ConsoleApplication3\\Debug\\ConsoleApplication3.exe"
 
 ### End Constants
 
@@ -36,12 +39,30 @@ def getPasswords(loginFile):
 	conn = sqlite3.connect(loginFile)
 
 	if loginFile is GCHROME_LOGIN_FILE:
-		query = "SELECT username_value, quote(password_value), origin_url FROM logins"
+		#chrome on windows, run Windows C application
+		if opsys == "Windows":
+			print "Attempting to decrypt Windows Chrome logins"
+
+			query = "SELECT origin_url, username_value, password_value FROM logins"
+			
+			for row in conn.execute(query):
+				pwd = base64.b64encode(row[2])
+				pwd = "ciao"
+				retval = subprocess.check_output([WIN_DECRYPTER, pwd])
+				print retval
+
+		#chrome on linux, clear text password or encrypted with key chain, simply return the value we found
+		elif opsys == "Linux":
+			query = "SELECT origin_url, username_value, quote(password_value) FROM logins"
+
+			for row in conn.execute(query):
+				print row
+
 	elif loginFile is MOZ_LOGIN_FILE_DB:
 		query = "SELECT hostname, encryptedUsername, encryptedPassword FROM moz_logins"
 
-	for row in conn.execute(query):
-		print row
+		#firefox/thunderbird on linux/windows, use NSS library to decrypt
+
 
 
 def readLoginsJSON(loginFileJSON):
@@ -81,22 +102,22 @@ def readMozillaLogins(sw, profileDir):
 ### Code
 
 #detect os and look in specific folders
-if platform.system() == "Linux":
+if opsys == "Linux":
 	chromeProfile = GCHROME_PROFILE_LINUX
 	ffProfile = FF_PROFILE_LINUX
 	thProfile = TH_PROFILE_LINUX
-elif platform.system() == "Windows":
-	chromeProfile = GCHROME_PROFILE_LINUX
-	ffProfile = FF_PROFILE_LINUX
-	thProfile = TH_PROFILE_LINUX
-	
+elif opsys == "Windows":
+	chromeProfile = GCHROME_PROFILE_WIN
+	ffProfile = FF_PROFILE_WIN
+	thProfile = TH_PROFILE_WIN
+
 #google-chrome
 os.chdir(chromeProfile)
 print "===> Attempting to read chrome login file"
 getPasswords(GCHROME_LOGIN_FILE)
 
 #firefox & thunderbird
-readMozillaLogins("firefox", ffProfile)
-readMozillaLogins("thunderbird", thProfile)
+#readMozillaLogins("firefox", ffProfile)
+#readMozillaLogins("thunderbird", thProfile)
 
 ### End Code
