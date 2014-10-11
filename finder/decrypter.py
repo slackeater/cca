d46823beb887c2345f518a7e4bf102dc72a33655
Giuseppe Scalzi <giuseppe@osgate.org>
2014-10-11 23:25:53 +0200

    Structural changes

--- finder/decrypter.py
+++ finder/decrypter.py
@@ -9,7 +9,9 @@
 ####
 
 from ctypes import *
-import getpass, platform, sqlite3, os, json, subprocess, base64, glob, hashlib
+import sqlite3, os, json, subprocess, base64, glob
+import crypto, logger
+from credentials import Credentials
 
 ### Code from ffpwdcracker.py
 
@@ -27,90 +29,80 @@ class secuPWData(Structure):
 
 ### Constants and globals
 
-HASH_SEPARATOR = "|"
-
 GCHROME_LOGIN_FILE = "Login Data"
 MOZ_LOGIN_FILE_DB = "signons.sqlite"
 MOZ_LOGIN_FILE_JSON = "logins.json"
-
-username = getpass.getuser()
-opsys = platform.system()
-libnss = "libnss3.so" if opsys == "Linux" else "nss3.dll"
-
-GCHROME_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Local\Google\Chrome\User Data"
-FF_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Roaming\Mozilla\Firefox"
-TH_PROFILE_WIN = "C:\Users\\" + username + "\AppData\Roaming\Thunderbird"
-
-GCHROME_PROFILE_LINUX = "/home/" + username + "/.config/google-chrome"
-FF_PROFILE_LINUX = "/home/" + username + "/.mozilla/firefox"
-TH_PROFILE_LINUX = "/home/" + username + "/.thunderbird"
-
 WIN_DECRYPTER = "C:\\Users\\John\\Documents\\Visual Studio 2012\\Projects\\ConsoleApplication3\\Debug\\ConsoleApplication3.exe"
-
-#detect os and set profile folders
-if opsys == "Linux":
-	chromeProfile = GCHROME_PROFILE_LINUX
-	ffProfile = FF_PROFILE_LINUX
-	thProfile = TH_PROFILE_LINUX
-elif opsys == "Windows":
-	chromeProfile = GCHROME_PROFILE_WIN
-	ffProfile = FF_PROFILE_WIN
-	thProfile = TH_PROFILE_WIN
 
 ### End Constants and globals
 
-### Def
-
-def getPasswords(loginFile):
+def getPasswords(loginFile, profile, nss = None, opsys = None):
 	""" Select password from relative SQLite database """
 
+	cred = list()
+
 	if not os.path.isfile(loginFile):
-		return None
+		logger.error("Login file " + loginFile + " does not exists.")
+		return cred
 
 	conn = sqlite3.connect(loginFile)
 
 	if loginFile is GCHROME_LOGIN_FILE:
 		#chrome on windows, run Windows C application
 		if opsys == "Windows":
-			print "Attempting to decrypt Windows Chrome logins"
+			logger.log("Attempting to decrypt Windows Chrome logins")
 
 			query = "SELECT origin_url, username_value, hex(password_value) FROM logins"
 
 			for row in conn.execute(query):
-				pwd = row[2]
-				#execute windows decrypt helper with password as parameter
-				retval = subprocess.check_output([WIN_DECRYPTER, pwd])
-				print retval
+				try:
+					pwd = row[2]
+					#execute windows decrypt helper with password as parameter
+					retval = subprocess.check_output([WIN_DECRYPTER, pwd])
+
+					if retval == "<decryption failed>":
+						pwd = ""
+					else:
+						pwd = retval
+
+					cred.append(Credentials(row[0], row[1], pwd, profile))
+				except subprocess.CalledProcessError as e:
+					logger.error(format(e.errno, e.strerror))
 
 		#chrome on linux, clear text password or encrypted with key chain, simply return the value we found
 		elif opsys == "Linux":
 			query = "SELECT origin_url, username_value, quote(password_value) FROM logins"
 
 			for row in conn.execute(query):
-				data = [row[0],row[1],row[2]]
-				print data
+				cred.append(Credentials(row[0], row[1], row[2], profile))
 
 	elif loginFile is MOZ_LOGIN_FILE_DB:
 		query = "SELECT hostname, encryptedUsername, encryptedPassword FROM moz_logins"
-
+		
 		for row in conn.execute(query):
-			#firefox/thunderbird on linux/windows, use NSS library to decrypt
-			credentials = decryptMozilla(row[1],row[2])
-			data = [row[0]] + credentials
-			print data
-			hash_object = hashlib.sha256(data[0] + HASH_SEPARATOR + data[1] + HASH_SEPARATOR + data[2])
-			print "Hash:" + hash_object.hexdigest()
+			  #firefox/thunderbird on linux/windows, use NSS library to decrypt
+			  credentials = decryptMozilla(row[1],row[2], nss)
+			  cred.append(Credentials(row[0], credentials[0], credentials[1], profile))	
 
 	conn.close()
+	return cred
 		
-def decryptMozilla(username, password):
+def decryptMozilla(username, password, libnss):
 	""" Decrypt mozilla login, based on ffpwdcracker.py """
+	
+	decItems = ["",""]
+
+	if libnss is None:
+	  logger.error("No nss given")
+	  return decItems
+	
 	# code from ffpwdcracker.py
 	nss = CDLL(libnss)
 	
 	if nss.NSS_Init(os.getcwd()) != 0:
-		print "Error initializing NSS"
-		return None
+		logger.error("Error initializing NSS")
+		logger.error(nss.PORT_GetError())
+		return decItems
 
 	#prepare data structures
 	pwdata = secuPWData()
@@ -129,39 +121,49 @@ def decryptMozilla(username, password):
 
 	#decrypt
 	if nss.PK11SDR_Decrypt(byref(uname),byref(dectext),byref(pwdata))==-1:
-		print nss.PORT_GetError()
-		print "Error decrypting"
-		return None
+		logger.error("Error decrypting")
+		logger.error(nss.PORT_GetError())
+		return decItems 
 
 	decUsername = string_at(dectext.data,dectext.len)
 
 	if nss.PK11SDR_Decrypt(byref(passwd),byref(dectext),byref(pwdata))==-1:
-		print nss.PORT_GetError()
-		print "Error decrypting"
-		return None
+		logger.error("Error decrypting")
+		logger.error(nss.PORT_GetError())
+		return decItems
 
 	decPassword = string_at(dectext.data,dectext.len)
 	
-	decitems = [decUsername, decPassword]
+	decItems = [decUsername, decPassword]
 	nss.NSS_Shutdown()
-	return decitems
+
+	return decItems
 	
-def readLoginsJSON(loginFileJSON):
+def readLoginsJSON(loginFileJSON, profile, nss):
 	""" Read the JSON of Firefox login data """
 
+	cred = list()
+
 	if not os.path.isfile(loginFileJSON):
-		return None
+		logger.error("JSON file " + loginFileJSON + " does not exist.")
+		return cred
 
 	data = json.loads(open(loginFileJSON).read())
 
-	credentials = decryptMozilla(data['logins'][0]['encryptedUsername'],data['logins'][0]['encryptedPassword'])
-	data = [data['logins'][0]['hostname']] + credentials
-	print data
+	for login in data['logins']:
+		credentials = decryptMozilla(login['encryptedUsername'],login['encryptedPassword'], nss)
+		cred.append(Credentials(login['hostname'], credentials[0], credentials[1], profile))	
 
-def readMozillaLogins(sw, profileDir):
+	return cred
+
+def readMozillaLogins(sw, profileDir, nss):
 	""" Read and parse the login file of thunderbird or firefox """
 
-	print "===> Attempting to read " + sw + " login file"
+	# initialize variable used for return 
+	cred = list()	
+
+	logger.log("\n","no")
+	logger.log(sw.upper())
 
 	#get first default profile directory
 	os.chdir(profileDir)
@@ -173,55 +175,49 @@ def readMozillaLogins(sw, profileDir):
 		# get profile directory
 		if line.startswith("Path="): 
 			p = line.split("=")[1].strip("\n")
-			print "Found profile directory " + p
+			logger.log("Found profile directory " + p)
 			os.chdir(p)
 
 			# look for signons.sqlite
-			getPasswords(MOZ_LOGIN_FILE_DB)
+			cred =  cred + getPasswords(MOZ_LOGIN_FILE_DB, p, nss)
 
 			# look for logins.json
-			readLoginsJSON(MOZ_LOGIN_FILE_JSON)
+			cred = cred + readLoginsJSON(MOZ_LOGIN_FILE_JSON, p, nss)
 
 			#go up one directory
 			os.chdir(os.path.dirname(os.getcwd()))
 
-def readChromeLogins():
-	""" Read and parse the logi n file of chrome """
+	return cred
 
-	print "===> Attempting to read chrome login file"
+def readChromeLogins(chromeProfile, opsys):
+	""" Read and parse the login file of chrome """
+	
+	# initialize variable used for return 
+	cred = list()	
+
+	logger.log("\n","no")
+	logger.log("GOOGLE CHROME")
+
 	os.chdir(chromeProfile)
 
 	#look in default profile
 	if os.path.isdir("Default"):
 		os.chdir("Default")
-		print "Found profile directory Default"
-		getPasswords(GCHROME_LOGIN_FILE)
+		logger.log("Found profile directory Default")
+		cred = cred + getPasswords(GCHROME_LOGIN_FILE, "Default", None, opsys)
 
 		#up one directory
 		os.chdir(os.path.dirname(os.getcwd()))
 
 	#look for other profiles (Profile 1, Profile 2, Profile 3, ...)
-	print os.getcwd()
-
 	profiles = glob.glob("Profile *")
 
 	for profile in profiles:
 		os.chdir(profile)
-		print "Found profile directory " + profile
-		getPasswords(GCHROME_LOGIN_FILE)
+		logger.log("Found profile directory " + profile)
+		cred = cred + getPasswords(GCHROME_LOGIN_FILE, profile, None, opsys)
 
 		#up one directory
 		os.chdir(os.path.dirname(os.getcwd()))
 
-### End Def
-
-### Code
-
-#google-chrome
-readChromeLogins()
-
-#firefox & thunderbird
-readMozillaLogins("firefox", ffProfile)
-readMozillaLogins("thunderbird", thProfile)
-
-### End Code
+	return cred
