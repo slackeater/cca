93c766d4e9cb59b2d9c0017806f7df177fd1074c
Giuseppe Scalzi <giuseppe@osgate.org>
2014-10-13 11:45:50 +0200

    Begin with file search

--- finder/browserfile.py
+++ finder/browserfile.py
@@ -15,15 +15,29 @@ from credentials import Credentials
 UNAME = getpass.getuser()
 OP_SYS = platform.system()
 
-GCHROME_PROFILE_WIN = r'C:\Users\\" + UNAME + "\AppData\Local\Google\Chrome\User Data'
-FF_PROFILE_WIN = r'C:\Users\\" + UNAME + "\AppData\Roaming\Mozilla\Firefox'
-TH_PROFILE_WIN = r'C:\Users\\" + UNAME + "\AppData\Roaming\Thunderbird'
+TH_PROFILE_WIN = 'C:\\Users\\' + UNAME + "\AppData\Roaming\Thunderbird"
 LIBNSS_WIN = "nss3.dll"
 
-GCHROME_PROFILE_LINUX = "/home/" + UNAME + "/.config/google-chrome"
-FF_PROFILE_LINUX = "/home/" + UNAME + "/.mozilla/firefox"
 TH_PROFILE_LINUX = "/home/" + UNAME + "/.thunderbird"
 LIBNSS_LINUX = "libnss3.so"
+
+# firefox useful file
+FF_PROFILE_WIN = 'C:\\Users\\' + UNAME + "\AppData\Roaming\Mozilla\Firefox"
+FF_PROFILE_LINUX = "/home/" + UNAME + "/.mozilla/firefox"
+COOKIES = "cookies.sqlite"
+FORM_HISTORY = "formhistory.sqlite"
+PLACES = "places.sqlite"
+MOZ_LOGIN_FILE_DB = "signons.sqlite"
+MOZ_LOGIN_FILE_JSON = "logins.json"
+
+# chrome useful file
+GCHROME_PROFILE_WIN = 'C:\Users\\' + UNAME + "\AppData\Local\Google\Chrome\User Data"
+GCHROME_PROFILE_LINUX = "/home/" + UNAME + "/.config/google-chrome"
+GCHROME_LOGIN_FILE = "Login Data"
+BOOKMARKS = "Bookmarks"
+COOKIES = "Cookies"
+HISTORY = "History"
+WEB_DATA = "Web Data"
 
 #detect os and set profile folders
 if OP_SYS == "Linux":
@@ -50,20 +64,36 @@ def chromeFinder():
 		logger.log(c.hostname + ", " + c.username + ", " + c.password + ", " + c.profile )
 		logger.log("=>" + c.signature)
 
-	
 
 def firefoxFinder():
 	""" Find useful file about firefox """
 	if not  os.path.isdir(ffProfile):
 		logger.log("No firefox profile folder found")
 	
-	ffCred = decrypter.readMozillaLogins("firefox",ffProfile, libnss)
+	# look in all profiles
+	os.chdir(ffProfile)
+	cred = list()
+
+	for profile in open("profiles.ini", "r"):
+		#get profile dir
+		if profile.startswith("Path="):
+			p = profile.split("=")[1].strip("\n")
+			logger.log("Found profile directory " + p)
+			os.chdir(p)
+
+			## passwords
+			cred = cred + decrypter.getPasswords(MOZ_LOGIN_FILE_DB,p,libnss)
+			cred = cred + decrypter.readLoginsJSON(MOZ_LOGIN_FILE_JSON, p, libnss)
+
+			##useful file
 
 	logger.log("\n === Credentials for firefox found: === ","no")
 
-	for c in ffCred:
+	for c in cred:
 		logger.log(c.hostname + ", " + c.username + ", " + c.password + ", " + c.profile )
 		logger.log("=>" + c.signature)
+
+
 
 def thunderbirdFinder():
 	""" Find useful file about thunderbird """
@@ -76,5 +106,10 @@ def thunderbirdFinder():
 	logger.log("\n === Credentials for thunderbird found: === ","no")
 
 	for c in thCred:
-	logger.log(c.hostname + ", " + c.username + ", " + c.password + ", " + c.profile )
-	logger.log("=>" + c.signature)
+		logger.log(c.hostname + ", " + c.username + ", " + c.password + ", " + c.profile )
+		logger.log("=>" + c.signature)
+
+
+#chromeFinder()
+firefoxFinder()
+#thunderbirdFinder()
