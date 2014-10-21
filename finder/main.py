40033a23a92e90be0d831181091bfc65946d0069
Giuseppe Scalzi <giuseppe@osgate.org>
2014-10-21 22:31:30 +0200

    Review

--- finder/main.py
+++ finder/main.py
@@ -10,40 +10,60 @@
 ####
 
 import cloud, browserfile, config, packetizer, crypto, logger, getpass
-import os, subprocess, time, json, zipfile
+import os, subprocess, time, json, zipfile, sys, base64
 
-def main():
-
+def init(reportPathUser):
+	""" Initialize report """
 	# report name and path
 	reportName = crypto.md5(str(time.time()))
-	completeReportPath = os.path.join(config.START_PATH, reportName)
 	
+	if os.path.isdir(reportPathUser):
+		reportPath = os.path.join(reportPathUser, reportName)	
+	elif reportPathUser is None:
+		reportPath = os.path.join(config.START_PATH, reportName)
+	else:
+		logger.log("Path " + reportPathUser + " is not a valid path")
+		sys.exit(1)
+
 	# create directory used for report files
-	os.mkdir(completeReportPath)
+	os.mkdir(reportPath)
+
+	logger.log("Report name: " + reportName)
+	logger.log("Path: " + reportPath)
+
+	return (reportName, reportPath)
+
+def browserFinder(reportPath):
+	""" Run the finder for the browsers """
 
 	# find passwords and browser files
-	chromeReportFolder = os.path.join(completeReportPath, config.GCHROME_COPY_FOLDER)
+	chromeReportFolder = os.path.join(reportPath, config.GCHROME_COPY_FOLDER)
+	logger.log("Chrome Profile Folder: " + chromeReportFolder)
 	os.mkdir(chromeReportFolder)
 	chrome = browserfile.chromeFinder(chromeReportFolder)
 
-	ffReportFolder = os.path.join(completeReportPath, config.FF_COPY_FOLDER)
+	ffReportFolder = os.path.join(reportPath, config.FF_COPY_FOLDER)
+	logger.log("Firefox Profile Folder: " + ffReportFolder)
 	os.mkdir(ffReportFolder)
 	ffList = browserfile.firefoxFinder(ffReportFolder)
 
-	thReportFolder = os.path.join(completeReportPath, config.TH_COPY_FOLDER)
+	thReportFolder = os.path.join(reportPath, config.TH_COPY_FOLDER)
+	logger.log("Thunderbird Profile Folder: " + thReportFolder)
 	os.mkdir(thReportFolder)
 	thList = browserfile.thunderbirdFinder(thReportFolder)
 
-	browserPackList = list()
-	browserPackList.append(chrome)
-	browserPackList.append(ffList)
-	browserPackList.append(thList)
+	browserPackList = [chrome, ffList, thList]
+	return browserPackList
 
+def cloudFinder():
+	""" Run the finder for the cloud """
+	
 	# find cloud files
 	cloudPackList = list()
 	dropboxList = cloud.dropbox()
 	cloudPackList.append(dropboxList)
 
+	# gdrive and onedrive are not available for linux
 	if config.OP_SYS == "Windows":
 		gdriveList = cloud.gdrive()
 		onedriveList = cloud.onedrive()
@@ -51,27 +71,32 @@ def main():
 		cloudPackList.append(gdriveList)
 		cloudPackList.append(onedriveList)
 
-	#pack all together
+	return cloudPackList	
+
+def zipper(encZipPath, reportName, reportPath, browserPackList, cloudPackList):
+	""" Create the directories and the encrypted ZIP """
+	
+	#pack all together insto JSON
 	jsontext = packetizer.mainPacker(browserPackList, cloudPackList)
+	
+	logger.log("\n === JSON Report === \n")
 	logger.log(jsontext, "no")
 
-	crypt = crypto.makeReport(crypto.encryptAES(jsontext))
-	
 	# save into a file
-	os.chdir(completeReportPath)
+	os.chdir(reportPath)
 	fileName = reportName + ".report"
 	f = open(fileName,"w+")
-	f.write(json.dumps(crypt, sort_keys=True, indent=4))
+	jsonReport = packetizer.mainPacker(browserPackList, cloudPackList)
+	f.write(json.dumps(jsonReport, sort_keys=True, indent=4))
 	f.close()
-
-	
-	# change to start directory if not already in it	
-	if os.getcwd() != config.START_PATH:
-		os.chdir(config.START_PATH)
+	logger.log("JSON report " + fileName + "  stored at " + os.getcwd())
 	
 	# create  ZIP 
-	zipFile = zipfile.ZipFile(reportName + ".zip", "w")
+	os.chdir(os.path.dirname(os.getcwd()))
+	zipFileName = reportName + ".zip"
+	zipFile = zipfile.ZipFile(zipFileName, "w")
 
+	# walk the directory and add to ZIP
 	for dirname, subdirs, files in os.walk(reportName):
 		zipFile.write(dirname)
 
@@ -79,19 +104,40 @@ def main():
 			zipFile.write(os.path.join(dirname, filename))
 	
 	zipFile.close()
+	logger.log("ZIP file " + zipFileName + " created in " + os.getcwd())
 		
-	logger.log("Report file written to " + os.path.join(config.START_PATH,reportName + ".zip"))
+	# now crypt the file
+	zipEnc = os.path.join(encZipPath, zipFileName + ".enc")
+	key = os.urandom(32)
+	logger.log("Encrypting " + os.path.join(encZipPath, zipFileName) + " to " + zipEnc)
+	crypto.encryptAESFile(os.path.join(encZipPath,zipFileName), zipEnc, key)
+
+	# compute HMAC
+	hmacHandler = open(zipEnc, "r")
+	hmacDigest = crypto.sha256File(hmacHandler, key)
+	hmacHandler.close()
+
+	# encrypt the key
+	encKey = base64.b64encode(crypto.encryptRSA(key))
 	
-	
-	# hmac signature of zip
-	pa = getpass.getpass("Password:")
-	
-	hmacDigest = crypto.sha256File(open(os.path.join(config.START_PATH,reportName + ".zip")), pa)
-	hmacFile = open(os.path.join(config.START_PATH, reportName + ".zip.sig"), "w+")
-	hmacFile.write(hmacDigest)
+	hmacFile = open(zipEnc + ".sig", "w+")
+	hmacFile.write("digest:" + hmacDigest)
+	hmacFile.write("\nk:" + encKey)
 	hmacFile.close()
 
-	# uncomment for final version
+	# delete the random key
+	key = None
+	del key
+	
+def main():
+	userPath = sys.argv[1]
+
+	report = init(userPath)
+	browserPackList = browserFinder(report[1])
+	cloudPackList = cloudFinder()
+	zipper(userPath, report[0], report[1], browserPackList, cloudPackList)
+	
+		# uncomment for final version
 	#os.rmdir(completeReportPath)
 
 if __name__ == "__main__":
