46e71d6c90c6da0f4599de13187b37ae2e51da85
Giuseppe Scalzi <giuseppe@osgate.org>
2014-11-26 11:51:06 +0100

    PDF diff

--- webapp/comparator/ajax.py
+++ webapp/comparator/ajax.py
@@ -3,7 +3,7 @@ from dajaxice.decorators import dajaxice_register
 from dajaxice.utils import deserialize_form
 from webapp.func import *
 import fileComparator
-from downloader.models import FileDownload
+from downloader.models import FileDownload, Download
 from django.template.loader import render_to_string
 import time
 @dajaxice_register
@@ -19,14 +19,18 @@ def compareTwoFile(request,revOne,revTwo,altName,cloudItem,tokenID):
 		ci = checkCloudItem(cloudItem,request.user.id)
 		tkn = checkAccessToken(t,ci)
 	
-		#get file alternate name from db
-		altNameDb = FileDownload.objects.get(tokenID=tkn,alternateName=altName)
+		#get file alternate name and file name from db
+		f = FileDownload.objects.get(tokenID=tkn,alternateName=altName)
 
-		fileComparator.compareTwo(revOne,revTwo,altNameDb.alternateName,tkn)
+		#get folder name
+		download = Download.objects.get(tokenID=tkn,threadStatus="completed")
+
+		diffName = fileComparator.compareTwo(str(revOne),str(revTwo),f,download.folder,tkn)
 		
-		iframe = "<iframe id='viewer' src ='/static/diff/diff.pdf' height='600' width='800' style='text-align: center'  allowfullscreen webkitallowfullscreen></iframe>"
+		iframe = "<embed src ='/diff/"+diffName+"' height='600' width='100%' style='text-align: center'>"
+		
 		dajax.assign("#comparator","innerHTML",iframe)
-	except:
+	except Exception as e:
 		dajax.assign("#comparatorError","innerHTML",e.message)
 
 	return dajax.json()
--- webapp/comparator/fileComparator.py
+++ webapp/comparator/fileComparator.py
@@ -1,5 +1,32 @@
-from downloader.models import FileDownload,FileHistory
+from django.conf import settings
+from downloader.models import FileDownload,FileHistory, Download
+import os,subprocess
 
+def compareTwo(revOneID,revTwoID,diffFile,downloadFolder,token):
 
-def compareTwo(revOne,revTwo,altName,token):
-	print "Compare me"
+	#diff file full path
+	diffPath = os.path.join(settings.DOWNLOAD_DIR,downloadFolder,"history",diffFile.alternateName)
+
+	#get the two file
+	revOnePath = os.path.join(diffPath,diffFile.fileName+"_"+revOneID)
+	revTwoPath = os.path.join(diffPath,diffFile.fileName+"_"+revTwoID)
+
+	#diff name
+	resultDiffName = "diff_"+str(token.id)+"_"+revOneID+"_"+revTwoID+".pdf"
+	
+	resultDiffPath = os.path.join(settings.DIFF_DIR,resultDiffName)
+
+	#check if a diff already exists for this file
+	if os.path.isfile(resultDiffPath):
+		return resultDiffName
+
+	#generate diff
+	try:
+		subprocess.check_output(["diff-pdf","--output-diff="+resultDiffPath+"",revOnePath,revTwoPath],stderr=subprocess.STDOUT)
+	except subprocess.CalledProcessError as e:
+		#necessary because diff-pdf always exits with 1, because of the message "No protocol specified" caused by the absence of the server X, even though the diff is generated
+		if e.output.strip() != "No protocol specified":
+			raise Exception("Error computing diff of PDF")
+	
+	return resultDiffName	
+
